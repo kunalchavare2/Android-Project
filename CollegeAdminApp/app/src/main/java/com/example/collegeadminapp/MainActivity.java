5f6ea3aebbcbd08596215c6f375654e30f5db71b
Kunal Chavare <kunalchavare2@gmail.com>
2021-01-24 22:57:57 +0530

    1.Application Connected to firebase 2.Added navigation to Upload Notice page 3.Added support to select image from gallery 4.Design and implemented ui for Upload Notice page

--- CollegeAdminApp/app/src/main/java/com/example/collegeadminapp/MainActivity.java
+++ CollegeAdminApp/app/src/main/java/com/example/collegeadminapp/MainActivity.java
@@ -1,15 +1,30 @@
 package com.example.collegeadminapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 public class MainActivity extends AppCompatActivity {
-
+    public CardView addNotice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+
+    public void addNotice(View view) {
+        addNotice = findViewById(R.id.addNotice);
+
+        Uri uri;
+        Intent intent = new Intent(this, UploadNotice.class);
+        startActivity(intent);
     }
 }
